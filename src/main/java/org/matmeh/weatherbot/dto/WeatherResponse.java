ed1240a31f3daf140338c692f7c49d966fe3a68f
ArtWolf <109514696+ArtVlk@users.noreply.github.com>
2023-12-16 19:06:41 +0500

    Add files via upload

--- src/main/java/org/matmeh/weatherbot/dto/WeatherResponse.java
+++ src/main/java/org/matmeh/weatherbot/dto/WeatherResponse.java
@@ -3,7 +3,12 @@ package org.matmeh.weatherbot.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.net.URL;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -25,4 +30,33 @@ public class WeatherResponse {
                 "Скорость ветра: " + wind.get("speed") + " м/с";
 
     }
+    public static String getWeatherFive(String city, StringBuffer response) {
+        JSONObject jsonObject = new JSONObject(response.toString());
+        JSONArray list = jsonObject.getJSONArray("list");
+        StringBuilder resultBuilder = new StringBuilder();
+        for (int i = 0; i < 40; i += 8) { // считывается прогноз каждые 3 часа, поэтому считываем только 1 раз в день
+            JSONObject dayForecast = list.getJSONObject(i);
+            String dateText = dayForecast.getString("dt_txt");
+            JSONObject main = dayForecast.getJSONObject("main");
+            double temp = main.getDouble("temp");
+            double feelsLike = main.getDouble("feels_like");
+            int pressure = (main.getInt("pressure"));
+            pressure = (int) (pressure * (0.75));
+            int humidity = main.getInt("humidity");
+            JSONArray weatherArray = dayForecast.getJSONArray("weather");
+            JSONObject weatherData = weatherArray.getJSONObject(0);
+            String weatherDescription = weatherData.getString("description");
+            JSONObject wind = dayForecast.getJSONObject("wind");
+            double windSpeed = wind.getDouble("speed");
+
+            resultBuilder.append("\nДата и время: " + dateText + "\n"
+                    + "Температура: " + temp + "C° (Ощущается, как " + feelsLike + "C°)" + "\n"
+                    + weatherDescription + "\n"
+                    + "Атмосферное давление: " + pressure + " мм.рт.ст.\n"
+                    + "Влажность воздуха: " + humidity + "%\n"
+                    + "Скорость ветра: " + windSpeed + " м/с\n");
+        }
+        return String.valueOf(resultBuilder);
+    }
+
 }
